3623ce1a77cb663c072f34a3977c2b506f7927e0
Yesid <yesidrodriguez305@gmail.com>
2022-06-23 12:16:39 -0500

    Clase 5. Strict mode

---
+++ src/scope/strict.js
@@ -0,0 +1,18 @@
+pi = 3.1416;
+console.log(pi);
+
+// Lo que hace javascript:
+// var pi;
+// pi = 3.1416;
+// console.log(pi);
+
+'use strict';
+pi = 3.1416; // error. pi is not defined
+console.log(pi);
+
+function myFunction() {
+  'use strict';
+  return pi = 3.1416; // error. pi is not defined
+}
+
+console.log(myFunction());
