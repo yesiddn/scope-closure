62ce535b3036829802364ca912d6107d25c9fe8c
Yesid <yesidrodriguez305@gmail.com>
2022-06-24 12:33:49 -0500

    Clase 8. Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+// var nameOfDog; // undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Bobby';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+  console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
